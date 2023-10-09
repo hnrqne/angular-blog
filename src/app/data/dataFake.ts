c489c57a6cf6c7a348bc5c038a7e7b92de42d64c
Henrique Sampaio <hnrqne@gmail.com>
2023-10-09 15:10:36 -0300

    content updates

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -1,14 +1,16 @@
 export const dataFake = [
   {
-    id: 1,
-    photo: 'aaa',
-    title: 'saiu novo filme',
-    description: 'ola',
+    id: '1',
+    title: 'NOVIDADES NA AREA',
+    description: 'Coisas boas e inimagináveis estão por vir!',
+    photoCover:
+      'https://www.unfe.org/wp-content/uploads/2019/04/SM-placeholder.png',
   },
   {
-    id: 2,
-    photo: 'aaa',
-    title: 'saiu novo filme',
-    description: 'ola',
+    id: '2',
+    title: 'Novos caminhos serão traçados',
+    description: 'bla bla bla',
+    photoCover:
+      'https://www.unfe.org/wp-content/uploads/2019/04/SM-placeholder.png',
   },
 ];
--- src/app/pages/content/content.component.ts
+++ src/app/pages/content/content.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { dataFake } from '../../data/dataFake';
 
 @Component({
   selector: 'app-content',
@@ -6,12 +8,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./content.component.css'],
 })
 export class ContentComponent implements OnInit {
-  photoCover: string =
-    'https://www.unfe.org/wp-content/uploads/2019/04/SM-placeholder.png';
-  contentTitle: string = 'NOTICIA EXEMPLO';
-  contentDescription: string = 'blabla blabla';
+  photoCover: string = '';
+  contentTitle: string = '';
+  contentDescription: string = '';
+  private id: string | null = '0';
 
-  constructor() {}
+  constructor(private route: ActivatedRoute) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.route.paramMap.subscribe((value) => (this.id = value.get('id')));
+
+    this.setValuesToComponent(this.id);
+  }
+
+  setValuesToComponent(id: string | null) {
+    const result = dataFake.filter((article) => article.id === id)[0];
+
+    this.contentTitle = result.title;
+    this.contentDescription = result.description;
+    this.photoCover = result.photoCover;
+
+    console.log('contentTitle:', this.contentTitle);
+    console.log('contentDescription:', this.contentDescription);
+    console.log('photoCover:', this.photoCover);
+  }
 }
